9451667e7d489f7ec151ad20aeaf7c9f2d84af9e
jiushicheng <tanhan199303@163.com>
2018-01-30 21:37:09 +0800

    Update MiPushMessageReceiver.java

--- src/main/java/com/duanglink/mipush/MiPushMessageReceiver.java
+++ src/main/java/com/duanglink/mipush/MiPushMessageReceiver.java
@@ -70,7 +70,7 @@ public class MiPushMessageReceiver extends PushMessageReceiver {
     @Override
     public void onNotificationMessageArrived(Context context, MiPushMessage message) {
         mMessage = message.getContent();
-        Log.i(TAG, "收到通知栏消息： " + mMessage);\
+        Log.i(TAG, "收到通知栏消息： " + mMessage);
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
